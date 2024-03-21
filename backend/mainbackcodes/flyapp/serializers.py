a55aa3cc313d2aff17ccb88749ff2bc52692dcd2
Mehdi Shamshirband <mehdi.shamshirband@mail.concordia.ca>
2024-03-21 13:18:26 -0400

    Adding Create/Read/Update/Delete functions for models.

--- backend/mainbackcodes/flyapp/serializers.py
+++ backend/mainbackcodes/flyapp/serializers.py
@@ -1,20 +1,20 @@
 from rest_framework import serializers
-from models import Flight, Hotel, Activity
+from .models import Flight, Hotel, Activity
 
 
-class BookSerializer(serializers.ModelSerializer):
+class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
 
 
-class BookSerializer(serializers.ModelSerializer):
+class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
         fields = '__all__'
 
 
-class BookSerializer(serializers.ModelSerializer):
+class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
         fields = '__all__'
--- backend/mainbackcodes/flyapp/views.py
+++ backend/mainbackcodes/flyapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Flight, Hotel, Activity
+from .serializers import ActivitySerializer, HotelSerializer, FlightSerializer
+
 
 # Create your views here.
+class Flights(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class Hotels(viewsets.ModelViewSet):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
+
+
+class Activities(viewsets.ModelViewSet):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
