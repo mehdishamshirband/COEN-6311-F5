44879964c49f96c1f9358f79af138922e854529c
Mehdi Shamshirband <mehdi.shamshirband@mail.concordia.ca>
2024-03-21 20:33:28 -0400

    Task 1.1.3 which is implementing the filtering

--- backend/mainbackcodes/flyapp/views.py
+++ backend/mainbackcodes/flyapp/views.py
@@ -8,17 +8,27 @@ class Flights(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
 
+    filterset_fields = {'airlinename': ['icontains'], 'price': ['lte', 'gte'], 'destinationairport': ['icontains'],
+                        'originairport': ['icontains'], 'departuredatetime': ['lte', 'gte'],
+                        'arrivaldatetime': ['lte', 'gte']}
+
 
 class Hotels(viewsets.ModelViewSet):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
+
+    filterset_fields = {'hotelname': ['icontains'], 'address': ['icontains'], 'priceperday': ['lte', 'gte']}
 
 
 class Activities(viewsets.ModelViewSet):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
 
+    filterset_fields = {'type': ['icontains'], 'name': ['icontains'], 'price': ['lte', 'gte'], 'grade': ['icontains']}
+
 
 class Packages(viewsets.ModelViewSet):
     queryset = Package.objects.all()
     serializer_class = PackageSerializer
+
+    filterset_fields = {'price': ['lte', 'gte'], 'name': ['icontains'], 'grade': ['icontains']}
