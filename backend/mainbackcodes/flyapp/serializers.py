3d4c5c63177159c4949fdd1217874d9cbf4143af
Shayan_Hashemiezey <h_shaya@live.concordia.ca>
2024-03-24 12:55:10 -0400

    Make put,paych,del method inaccessible in package view

--- backend/mainbackcodes/flyapp/serializers.py
+++ backend/mainbackcodes/flyapp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Flight, Hotel, Activity, Package, Booking
+from .models import Flight,Hotel,Activity, Package,Booking
 
 
 class FlightSerializer(serializers.ModelSerializer):
