aa472c301ae328a072111eed61f85f260ebbfda7
Benjamin <b_vauche@live.concordia.ca>
2024-03-25 18:13:23 -0400

    Cleared cache

--- backend/mainbackcodes/flyapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Hotel,Flight,Activity,Package,Booking
-# Register your model, Accessible in admin site
-admin.site.register(Hotel)
-admin.site.register(Flight)
-admin.site.register(Activity)
-admin.site.register(Package)
-admin.site.register(Booking)
