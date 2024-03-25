891353401dbc6bcc3c333712ec756ce0a1fa5164
Benjamin <b_vauche@live.concordia.ca>
2024-03-25 18:20:02 -0400

    Cleared cache and changed gitignore

---
+++ backend/mainbackcodes/flyapp/models.py
@@ -0,0 +1,140 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Flight(models.Model):
+    airlinename = models.CharField(max_length=255)
+    flightid = models.IntegerField()
+    price = models.FloatField()
+    flightnumber = models.IntegerField()
+    originairport = models.CharField(max_length=255)
+    destinationairport = models.CharField(max_length=255)
+    availableseats = models.IntegerField()
+    departuredatetime = models.DateTimeField()
+    arrivaldatetime = models.DateTimeField()
+
+    class Meta:
+        ordering = ["airlinename"]
+
+    def __str__(self):
+        return self.airlinename
+
+
+class Hotel(models.Model):
+    hotelname = models.CharField(max_length=255)
+    hotelid = models.IntegerField()
+    address = models.CharField(max_length=255)
+    phone = models.IntegerField()
+    email = models.EmailField()
+    stars = models.IntegerField()
+    priceperday = models.FloatField()
+    capacity = models.IntegerField(default=5)
+    checkintime = models.DateField()
+    checkouttime = models.DateField()
+
+    class Meta:
+        ordering = ["hotelname"]
+
+    def __str__(self):
+        return self.hotelname
+
+
+grade = (
+    ('a', 'A'),
+    ('b', 'B'),
+    ('c', 'C')
+)
+
+
+class Activity(models.Model):
+    type = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.CharField(max_length=255)
+    grade = models.CharField(max_length=255, choices=grade)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+types = (
+    ('pre-made', 'PRE-MADE'),
+    ('custom', 'CUSTOM')
+)
+
+
+class Package(models.Model):
+    # ignore type field in ui (only used in backend)
+    type = models.CharField(max_length=255, choices=types, blank=True, default="CUSTOM")
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.CharField(max_length=255)
+    grade = models.CharField(max_length=255, choices=types)
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, null=True, blank=True)
+    activity = models.ManyToManyField(Activity, null=True, blank=True)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, null=True, blank=True)
+    start = models.DateField()
+    end = models.DateField()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+status = (
+    ('created', 'CREATED'),
+    ('pending', 'PENDING'),
+    ('canceled', 'CANCELED'),
+    ('modified', 'MODIFIED'),
+    ('checked', 'CHECKED')
+)
+
+
+class Booking(models.Model):
+    bookingid = models.IntegerField(unique=True)
+    customer = models.CharField(max_length=255)
+    totalcost = models.FloatField()
+    details = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, choices=status, default='CREATED')
+    package = models.ManyToManyField(Package)
+    creationdate = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["bookingid"]
+
+    def __str__(self):
+        return str(self.bookingid)
+
+
+state = (
+    ('pending', 'PENDING'),
+    ('rejected', 'REJECTED'),
+    ('accepted', 'ACCEPTED')
+)
+
+
+class PackageModification(models.Model):
+    name = models.CharField(max_length=255)
+    agent = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    state = models.CharField(max_length=255, choices=state, default='PENDING')
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, null=True, blank=True)
+    activity = models.ManyToManyField(Activity, blank=True)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, null=True, blank=True)
+    start = models.DateField()
+    end = models.DateField()
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    booking_cancellation = models.BooleanField(default=False, null=True, blank=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
---
+++ backend/mainbackcodes/mainbackcodes/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for mainbackcodes project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from flyapp import views
+
+router = routers.DefaultRouter()
+router.register(r'Flight', views.Flights, basename='flights')
+router.register(r'Hotel', views.Hotels, basename='hotels')
+router.register(r'Activity', views.Activities, basename='activities')
+router.register(r'Packages', views.Packages, basename='packages')
+router.register(r'Booking', views.BookingDetail, basename='booking')
+router.register(r'PackagesModifications', views.PackagesModification, basename='packagesmodification')
+router.register(r'Report', views.Reports, basename='report')
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
+urlpatterns += router.urls
