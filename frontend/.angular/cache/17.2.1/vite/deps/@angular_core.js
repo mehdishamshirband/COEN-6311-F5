7b3f1b6e4abc39d7f6eb8320a9f9649876f1f1f9
Benjamin <b_vauche@live.concordia.ca>
2024-03-25 22:59:12 -0400

    node_modules readded

--- frontend/.angular/cache/17.2.1/vite/deps/@angular_core.js
+++ frontend/.angular/cache/17.2.1/vite/deps/@angular_core.js
@@ -445,10 +445,10 @@ import {
   ɵɵvalidateIframeAttribute,
   ɵɵviewQuery,
   ɵɵviewQuerySignal
-} from "./chunk-QHIF2FQR.js";
-import "./chunk-V2DXGMIT.js";
-import "./chunk-UKEHM6V6.js";
-import "./chunk-ZDOIMVJD.js";
+} from "./chunk-JAT24HIO.js";
+import "./chunk-YIAFZ24P.js";
+import "./chunk-DGSSWR5S.js";
+import "./chunk-RDY66B4C.js";
 export {
   ANIMATION_MODULE_TYPE,
   APP_BOOTSTRAP_LISTENER,
