1f78d07231f768a6a39d31f5757f2da25f28a6db
Mehdi Shamshirband <mehdi.shamshirband@mail.concordia.ca>
2024-03-28 10:55:21 -0400

    Modification in order to merge with front end.

--- backend/mainbackcodes/flyapp/models.py
+++ backend/mainbackcodes/flyapp/models.py
@@ -4,47 +4,58 @@ from django.db import models
 # Create your models here.
 
 class Flight(models.Model):
-    airlinename = models.CharField(max_length=255)
-    flightid = models.IntegerField()
+    airline = models.CharField(max_length=255)
+    airlineLogo = models.CharField(max_length=255)
     price = models.FloatField()
-    flightnumber = models.IntegerField()
-    originairport = models.CharField(max_length=255)
-    destinationairport = models.CharField(max_length=255)
-    availableseats = models.IntegerField()
-    departuredatetime = models.DateTimeField()
-    arrivaldatetime = models.DateTimeField()
+    duration = models.IntegerField()
+    departureAirport = models.CharField(max_length=255)
+    departureCity = models.CharField(max_length=255)
+    departureCountry = models.CharField(max_length=255)
+    arrivalAirport = models.CharField(max_length=255)
+    arrivalCity = models.CharField(max_length=255)
+    arrivalCountry = models.CharField(max_length=255)
+    departureDate = models.DateTimeField()
+    arrivalDate = models.DateTimeField()
+    # stops = models.ManyToManyField(Flight)
+    showDetails = models.BooleanField(default=True, blank=True)
 
     class Meta:
-        ordering = ["airlinename"]
+        ordering = ["airline"]
 
     def __str__(self):
-        return self.airlinename
+        return self.airline
+
+
+class Photo(models.Model):
+    url = models.CharField(max_length=255)
+    caption = models.CharField(max_length=255, null=True, blank=True)
 
 
 class Hotel(models.Model):
-    hotelname = models.CharField(max_length=255)
-    hotelid = models.IntegerField()
-    address = models.CharField(max_length=255)
-    phone = models.IntegerField()
-    email = models.EmailField()
-    stars = models.IntegerField()
-    priceperday = models.FloatField()
-    capacity = models.IntegerField(default=5)
-    checkintime = models.DateField()
-    checkouttime = models.DateField()
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    website = models.CharField(max_length=255, null=True)
+    photos = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
-        ordering = ["hotelname"]
+        ordering = ["name"]
 
     def __str__(self):
-        return self.hotelname
+        return self.name
 
 
-grade = (
-    ('a', 'A'),
-    ('b', 'B'),
-    ('c', 'C')
-)
+class HotelBooking(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    totalPrice = models.FloatField()
+    checkIn = models.DateField()
+    checkOut = models.DateField()
+    showDetails = models.BooleanField(default=True, blank=True)
+
+    class Meta:
+        ordering = ["hotel"]
+
+    def __str__(self):
+        return str(self.hotel)
 
 
 class Activity(models.Model):
@@ -52,7 +63,10 @@ class Activity(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     description = models.CharField(max_length=255)
-    grade = models.CharField(max_length=255, choices=grade)
+    location = models.CharField(max_length=255)
+    date = models.DateField()
+    photos = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True, blank=True)
+    showDetails = models.BooleanField(default=True, blank=True)
 
     class Meta:
         ordering = ["name"]
@@ -67,18 +81,21 @@ types = (
 )
 
 
-class Package(models.Model):
+class TravelPackage(models.Model):
     # ignore type field in ui (only used in backend)
     type = models.CharField(max_length=255, choices=types, blank=True, default="CUSTOM")
     name = models.CharField(max_length=255)
     price = models.FloatField()
     description = models.CharField(max_length=255)
-    grade = models.CharField(max_length=255, choices=grade)
-    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, null=True, blank=True)
-    activity = models.ManyToManyField(Activity, null=True, blank=True)
-    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, null=True, blank=True)
-    start = models.DateField()
-    end = models.DateField()
+    flight = models.ManyToManyField(Flight, blank=True)
+    activity = models.ManyToManyField(Activity, blank=True)
+    hotel = models.ManyToManyField(HotelBooking, blank=True)
+    startingDate = models.DateField()
+    endingDate = models.DateField()
+    photos = models.ManyToManyField(Photo, blank=True)
+    showDetails = models.BooleanField(default=True, blank=True)
+    nbr_adult = models.IntegerField()
+    nbr_child = models.IntegerField()
 
     class Meta:
         ordering = ["name"]
@@ -87,29 +104,72 @@ class Package(models.Model):
         return self.name
 
 
+PaymentType = (
+    ('visa', 'VISA'),
+    ('paypal', 'PAYPAL'),
+)
+
+PaymentState = (
+    ('firstdeposit', 'FIRSTDEPOSIT'),
+    ('seconddeposit', 'SECONDDEPOSIT'),
+    ('lastdeposit', 'LASTDEPOSIT'),
+)
+
+
+class Billing(models.Model):
+    paymentType = models.CharField(max_length=255, choices=PaymentType)
+    paymentState = models.CharField(max_length=255, choices=PaymentState)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    firstLineAddress = models.CharField(max_length=255)
+    secondLineAddress = models.CharField(max_length=255)
+    zipCode = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state_area = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["firstName"]
+
+    def __str__(self):
+        return self.firstName
+
+
 status = (
     ('created', 'CREATED'),
-    ('pending', 'PENDING'),
+    ('processing', 'PROCESSING'),
     ('canceled', 'CANCELED'),
+    ('failed', 'FAILED'),
     ('modified', 'MODIFIED'),
-    ('checked', 'CHECKED')
+    ('confirmed', 'CONFIRMED'),
+    ('refunded', 'REFUNDED')
 )
 
 
 class Booking(models.Model):
-    bookingid = models.IntegerField(unique=True)
-    customer = models.CharField(max_length=255)
-    totalcost = models.FloatField()
+    bookingNo = models.IntegerField(unique=True)
+    cost = models.FloatField()
     details = models.CharField(max_length=255)
-    status = models.CharField(max_length=255, choices=status, default='CREATED')
-    package = models.ManyToManyField(Package)
-    creationdate = models.DateTimeField(auto_now_add=True)
+    billing = models.ForeignKey(Billing, on_delete=models.CASCADE)
+    bookingState = models.CharField(max_length=255, choices=status, default='CREATED')
+    travelPackage = models.ManyToManyField(TravelPackage)
+    purchaseDate = models.DateTimeField(auto_now_add=True)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    firstLineAddress = models.CharField(max_length=255)
+    secondLineAddress = models.CharField(max_length=255)
+    zipCode = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state_area = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
 
     class Meta:
-        ordering = ["bookingid"]
+        ordering = ["bookingNo"]
 
     def __str__(self):
-        return str(self.bookingid)
+        return str(self.bookingNo)
 
 
 state = (
@@ -125,12 +185,13 @@ class PackageModification(models.Model):
     price = models.FloatField()
     description = models.TextField()
     state = models.CharField(max_length=255, choices=state, default='PENDING')
-    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, null=True, blank=True)
+    flight = models.ManyToManyField(Flight, blank=True)
     activity = models.ManyToManyField(Activity, blank=True)
-    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, null=True, blank=True)
-    start = models.DateField()
-    end = models.DateField()
-    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    hotel = models.ManyToManyField(HotelBooking, blank=True)
+    startingDate = models.DateField()
+    endingDate = models.DateField()
+    photos = models.ManyToManyField(Photo, blank=True)
+    package = models.ForeignKey(TravelPackage, on_delete=models.CASCADE)
     booking_cancellation = models.BooleanField(default=False, null=True, blank=True)
 
     class Meta:
